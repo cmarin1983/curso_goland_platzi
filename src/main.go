734ef6eff9d02471ddaeffa7ac20fda1a5a3662c
Christian Marin <christianmarinp@outlook.com>
2021-09-23 11:50:02 -0500

    Hasta ciclo for

--- src/main.go
+++ src/main.go
@@ -4,17 +4,98 @@ import "fmt"
 
 func main() {
 
-	//Llama a función para ver las variables
-	variables()
+	fmt.Println("Este es el curso de Platzi.")
+	fmt.Println("Elegir el número cuál de las funcionalidades implementadas quieres ejecutar.")
+	fmt.Println("1: Variables")
+	fmt.Println("2: Aritmética")
+	fmt.Println("3: Librería fmt")
+	fmt.Println("4: Funciones")
+	fmt.Println("5: Ciclo FOR")
+	fmt.Println("6: Condicionales")
+	fmt.Print("Opción: ")
+	var n int
+	fmt.Scanln(&n)
 
-	//Llama a función de aritmética
-	aritmetica()
+	switch n {
+	case 1:
+		//Llama a función para ver las variables
+		variables()
+	case 2:
+		//Llama a función de aritmética
+		aritmetica()
+	case 3:
+		//Libreria fmt
+		libreriaFmt("Platzi")
+	case 4:
+		//Funciones
+		funciones()
+	case 5:
+		//Ciclo FOR
+		ciclos()
+	}
 
-	//Libreria fmt
-	libreriaFmt()
 }
 
-func libreriaFmt() {
+func ciclos() {
+
+	//En reversa
+	counterReverse := 10
+	for counterReverse >= 0 {
+		fmt.Println(counterReverse)
+		counterReverse--
+	}
+
+	//Condicional for
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+
+	//For while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	//For forever
+	/*counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}*/
+}
+
+func funciones() {
+	normalFuncion("Hola mundo.")
+	tripleFuncion(45, 88, "números")
+
+	value := returnFuncion(4)
+	fmt.Println("Valor es: ", value)
+
+	valor1, valor2 := dosValores(4)
+	fmt.Println("Valor1 y Valor2 son:", valor1, valor2)
+
+	valor3, _ := dosValores(4)
+	fmt.Println("Valor3 es:", valor3)
+}
+
+func normalFuncion(message string) {
+	fmt.Println(message)
+}
+
+func tripleFuncion(a, b int, c string) {
+	fmt.Printf("El %d y %d son %s.\n", a, b, c)
+}
+
+func returnFuncion(a int) int {
+	return a * 2
+}
+
+func dosValores(a int) (c, d int) {
+	return a, a * 2
+}
+
+func libreriaFmt(mensaje string) {
 	//Declaración de variables
 	helloMessage := "Hello"
 	worldMessage := "world"
@@ -36,6 +117,11 @@ func libreriaFmt() {
 	//Tipo de dato
 	fmt.Printf("helloMessge: %T\n", helloMessage)
 	fmt.Printf("cursos: %T\n", cursos)
+
+	fmt.Println("Cuál es el nomnre de este curso?")
+	var s string = mensaje
+	//fmt.Scanln(&s)
+	fmt.Printf("El nombre del curso es %s\n", s)
 }
 
 func variables() {
